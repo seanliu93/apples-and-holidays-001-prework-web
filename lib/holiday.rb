a96cae2924efeedc8830422351948dc2925f5842
seanliu93 <sean93liu@gmail.com>
2015-11-29 21:38:28 -0500

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -2,6 +2,68 @@ require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
   # given that holiday_hash looks like this:
+   #  {
+   #    :winter => {
+   #     :christmas => ["Lights", "Wreath"],
+   #     :new_years => ["Party Hats"]
+   #   },
+   #   :summer => {
+   #     :fourth_of_july => ["Fireworks", "BBQ"]
+   #   },
+   #   :fall => {
+   #     :thanksgiving => ["Turkey"]
+   #   },
+   #   :spring => {
+   #     :memorial_day => ["BBQ"]
+   #   }
+   # }
+  holiday_supplies[:summer][:fourth_of_july][1]
+  # return the second element in the 4th of July array
+end
+
+def add_supply_to_winter_holidays(holiday_hash, supply)
+  # holiday_hash is identical to the one above 
+  # add the second argument, which is a supply, to BOTH the 
+  # Christmas AND the New Year's arrays
+  holiday_hash[:winter].collect {|key, array|
+    array << supply
+    print array
+  }
+  holiday_hash
+end
+ 
+
+def add_supply_to_memorial_day(holiday_hash, supply)
+  # again, holiday_hash is the same as the ones above
+  # add the second argument to the memorial day array
+  holiday_hash[:spring][:memorial_day] << supply
+  holiday_hash
+  
+end
+
+def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+  # code here
+  # remember to return the updated hash
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
+  
+end
+
+def all_winter_holiday_supplies(holiday_hash)
+  # return an array of all of the supplies that are used in the winter season
+  holiday_hash[:winter].values.flatten
+  
+end
+
+def all_supplies_in_holidays(holiday_hash)
+  # iterate through holiday_hash and print items such that your readout resembles:
+  # Winter:
+  #   Christmas: Lights, Wreath
+  #   New Years: Party Hats
+  # Summer:
+  #   Fourth Of July: Fireworks, BBQ
+  # etc.
+  # given that holiday_hash looks like this:
   # {
   #   :winter => {
   #     :christmas => ["Lights", "Wreath"],
@@ -17,49 +79,46 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :memorial_day => ["BBQ"]
   #   }
   # }
-  # return the second element in the 4th of July array
-end
 
-def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above 
-  # add the second argument, which is a supply, to BOTH the 
-  # Christmas AND the New Year's arrays
-  
-end
- 
-
-def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
-  
-end
-
-def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
-  
-end
-
-def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
-  
-end
-
-def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
+  holiday_hash.each {|season, holidays|
+    puts season.to_s.capitalize << ":"
+    holidays.each {|holiday, array|
+      parsed_strs = holiday.to_s.split("_")
+      parsed_strs = parsed_strs.collect {|item|
+        item.capitalize
+      }
+      mystr = "  " << parsed_strs.join(" ") << ": "
+      #mystr = "  " << holiday.to_s.capitalize << ": "
+      if array.length == 2
+        mystr << array[0] << ", " << array[1]
+      elsif array.length == 1
+        mystr << array[0]
+      end
+      puts mystr
+      }
+    }
   
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  new_array = []
+  holiday_hash.each {|season, holidays|
+    holidays.each {|holiday, array|
+      hasBBQ = false
+      array.each {|item|
+        if item == "BBQ"
+          hasBBQ = true
+        end
+      }
+      if hasBBQ
+        new_array << holiday
+      end
 
+    }
+  }
+  new_array
 end
 
 
